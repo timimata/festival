bfe20b82546e30e05ca7d1c604100dd051dcc403
Tiago Machado <68192462+timimata@users.noreply.github.com>
2026-04-22 21:12:17 +0000

    changes feitas e tudo a correr

--- festival/urls.py
+++ festival/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('palcos/', views.palcos_view, name='palcos'),
+    path('dias/', views.dias_view, name='dias'),
     path('concertos/<int:id>/', views.concerto_view, name='concerto'),
 ]
--- festival/views.py
+++ festival/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Palco
+from .models import Dia
+from .models import Concerto
 
 
 def index_view(request):
@@ -13,10 +15,16 @@ def dias_view(request):
 
     return render(request, 'festival/dias.html', context)
 
+def palcos_view(request): # criada a função para mostrar os palcos
+    palcos = Palco.objects.all() 
+
+    context = {'palcos': palcos} # criada a variavel para passar os palcos para o template
+
+    return render(request, 'festival/palcos.html', context) # criada a função para mostrar os palcos, renderizando o template palcos.html e passando os palcos para o template
 
 
 def concerto_view(request, id):
-    concerto = 
+    concerto = Concerto.objects.get(id=id)
 
     context = {'concerto': concerto}
 
